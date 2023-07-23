281f1108a10bd792620423e52819201fc1ef83e2
tyler1588 <87100297+tyler1588@users.noreply.github.com>
2023-07-23 15:11:54 -0400

    complete 5.1

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -3,28 +3,37 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter.post('/', async (request, response) => {
-    const {username, password} = request.body
+loginRouter.post('/', async (request, response, next) => {
 
-    const user = await User.findOne({username})
-    const passwordCorrect = user === null 
-        ? false 
-        : await bcrypt.compare(password, user.passwordHash)
+    try {
+        const {username, password} = request.body
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({
-            error: 'invalid username or password'
-        })
+        const user = await User.findOne({username})
+        const passwordCorrect = user === null 
+            ? false 
+            : await bcrypt.compare(password, user.passwordHash)
+
+        if (!(user && passwordCorrect)) {
+            return response.status(401).json({
+                error: 'invalid username or password'
+            })
+        }
+
+
+        const userForToken = {
+            username: user.username,
+            id: user._id
+        }
+
+        const token = jwt.sign(userForToken, process.env.SECRET)
+        
+        response.status(200).send({token, username: user.username, name: user.name})
     }
 
-    const userForToken = {
-        username: user.username,
-        id: user._id
+    catch (exception) {
+        next(exception)
     }
-
-    const token = jwt.sign(userForToken, process.env.SECRET)
-
-    response.status(200).send({token, username: user.username, name: user.name})
+    
 })
 
 module.exports = loginRouter
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +16,80 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log("wrong credentials")
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)}/>
+          password
+          <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)}/>
+        </div>
+        <button type="submit">login</button>
+    </form>
+    )
+  }
+
+  const addBlog = event => {
+    event.preventDefault()
+    console.log("blog",   newBlog)
+    console.log("username", username)
+    console.log("password", password)
+  }
+
+  const blogForm = () => {
+    return (
+      <div>
+        <p>{user.name} logged in</p>
+        <form onSubmit={addBlog}>
+          <input
+            value={newBlog}
+            onChange={({target}) => setNewBlog(target.value)}
+          />
+          <button type="submit">save</button>
+        </form>  
+      </div>
+      
+    )
+  }
+
+  const display = () => {
+    if (user === null) {
+      return (
+        <>
+          <h2>Log in to application</h2>
+          {loginForm()}
+        </>
+      )
+    }
+    return (
+      <>
+        <h2>blogs</h2>
+        {blogForm()}
+        {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+        )}
+      </>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {display()}     
     </div>
   )
 }
